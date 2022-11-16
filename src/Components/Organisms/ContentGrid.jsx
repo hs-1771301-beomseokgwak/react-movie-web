0bf51b7a8fb50f89e4a3574508243fdcdd2a84b0
MintIsToothpaste <100202703+MintIsToothpaste@users.noreply.github.com>
2022-11-16 15:44:55 +0900

    UserPage Grid

--- src/Components/Organisms/ContentGrid.jsx
+++ src/Components/Organisms/ContentGrid.jsx
@@ -1,48 +1,70 @@
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import ContentCard from '../Molecules/ContentCard'
 import { findCountry } from '../../Modules/utils'
+import ContentCard from '../Molecules/ContentCard'
+import ContentSlideSectionTitle from '../Atoms/ContentSlideSectionTitle'
+import ContentSlideSectionLink from '../Atoms/ContentSlideSectionLink'
+import SkeletonContentSlide from './SkeletonContentSlide'
 
-const ContentGridDiv = styled.div`
-    position: relative;
-    display: grid;
-    width: 1280rem;
-    grid-template-columns: repeat(6, 1fr);
-    left: 50%;
-    transform: translateX(-50%);
-    row-gap: 30rem;
-    column-gap: 40rem;
+const ContentSlideSectionDiv = styled.div`
 `
 
-function ContentGrid({ data, showModal }) {
-    if (data != undefined) {
-        return (
-            <ContentGridDiv>
-                {data.map((element, index) => {
-                    let year1 = element.release_date || ''
-                    let year2 = year1?.slice(0, 4)
-                    let desc = `${year2}`
-                    let country
-                    try {
-                        country = element.production_countries[0].iso_3166_1
-                        desc += findCountry(country)
-                    } catch (error) {}
-                    return (
-                        <ContentCard
-                            key={`grid-content-${element.id}`}
-                            id={element.id}
-                            title={element.title}
-                            desc={desc}
-                            score={element.vote_average * 10}
-                            posterUrl={element.poster_path}
-                            index={index}
-                            type={'movie'}
-                            showModal={showModal}
-                        />
-                    )
-                })}
-            </ContentGridDiv>
-        )
+const ContentSlideGrid = styled.div`
+    position: absolute;
+    top: 0rem;
+    column-gap: 40rem;
+    transition: all 0.5s;
+    @media (min-width: 1380px) {
+        & {
+            width: 1280rem;
+        }
     }
+`
+
+const SlideWrapper = styled.div`
+    height: 1280rem;
+    width: 1280rem;
+    position: relative;
+`
+
+function ContentGrid({ sectionTitle, datas, showModal }) {
+    return (
+        <ContentSlideSectionDiv>
+            <div>
+                <ContentSlideSectionTitle text={sectionTitle} />
+            </div>
+                <SlideWrapper >
+                        <ContentSlideGrid id='contentSlideGrid' className='gr'>
+                            {datas.map((element, index) => {
+                                let rate1 = element.vote_average || ''
+                                let rate2 = Math.floor(rate1 * 10)
+                                let year1 = element.release_date || ''
+                                let year2 = year1?.slice(0, 4)
+                                let desc = `${year2}`
+                                let country
+                                try {
+                                    country = element.production_countries[0].iso_3166_1
+                                    desc += findCountry(country)
+                                } catch (error) {}
+                                return (
+                                        <ContentCard
+                                            key={`grid-content-${element.id}`}
+                                            id={element.id}
+                                            title={element.title}
+                                            desc={desc}
+                                            score={element.vote_average * 10}
+                                            posterUrl={element.poster_path}
+                                            index={index + 1}
+                                            type={'movie'}
+                                            showModal={showModal}
+                                        />
+                                )
+                            })}
+                        </ContentSlideGrid>
+                </SlideWrapper>
+        </ContentSlideSectionDiv>
+        
+    )
 }
 
 export default ContentGrid
--- src/Components/Templates/UserPageTemplate.jsx
+++ src/Components/Templates/UserPageTemplate.jsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
-import MovieSlideSection from '../Organisms/ContentSlideSection'
+import MovieSlideSection from '../Organisms/ContentGrid'
 import MainHeader from '../Organisms/MainHeader'
 import SubHeader from '../Organisms/SubHeader'
+import ModalDetailContent from '../Organisms/ModalDetailContent'
+import { useState, useEffect } from 'react'
 
 import MainSection from '../Organisms/MainSection'
 
@@ -9,7 +11,27 @@ const UserPageTemplateWrapper = styled.div`
     width: 100vw;
 `
 
-function UserPageTemplate({ trendMovies, trendTvs, gbsPick }) {
+function UserPageTemplate({ gbsPick }) {
+    const [modal, setModal] = useState(false)
+    const [noScroll, setScroll] = useState(false)
+    const [id, setId] = useState(null)
+
+    const showModal = async (id) => {
+        setModal(true)
+        setScroll(true)
+        setId(id)
+        document.body.style.overflow = 'none'
+    }
+
+    const hideModal = (async) => {
+        setModal(false)
+        setScroll(false)
+    }
+
+    useEffect(() => {
+        document.querySelector('html').style.overflowY = noScroll ? 'hidden' : 'auto'
+    })
+
     return (
         <>
             <SubHeader />
@@ -18,9 +40,10 @@ function UserPageTemplate({ trendMovies, trendTvs, gbsPick }) {
             <UserPageTemplateWrapper className='fc fleft'>
                 <MainSection />
                 <div className='fr fcenter'>
-                    <MovieSlideSection sectionTitle={'👍 곽범석님이 좋아하는 컨텐츠'} datas={gbsPick} type='movie'></MovieSlideSection>
+                    <MovieSlideSection datas={gbsPick} type='movie'></MovieSlideSection>
                 </div>
             </UserPageTemplateWrapper>
+            {modal ? <ModalDetailContent id={id} hideModal={hideModal} /> : null}
         </>
     )
 }
